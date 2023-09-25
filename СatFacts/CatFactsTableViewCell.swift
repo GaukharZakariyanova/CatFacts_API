11b7112d1db58eb71f24dae89c1b092aa692e22d
Gaukhar <zakariyanovagk@mail.ru>
2023-09-25 12:21:51 +0600

    first commit

---
+++ СatFacts/CatFactsTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CatFactsTableViewCell.swift
+//  СatFacts
+//
+//  Created by Zakariyanova Gaukhar on 31.07.2023.
+//
+
+import UIKit
+
+class CatFactsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ СatFacts/ListOfData.swift
@@ -0,0 +1,8 @@
+//
+//  ListOfData.swift
+//  СatFacts
+//
+//  Created by Zakariyanova Gaukhar on 31.07.2023.
+//
+
+import Foundation
